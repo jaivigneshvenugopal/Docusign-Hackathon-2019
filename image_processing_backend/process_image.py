bcecba09944f537c97af2522b71ace5bdda05c32
Jaivignesh Venugopal <smugs1995@gmail.com>
2019-06-08 15:10:26 -0700

    convert image to grayscale

--- image_processing_backend/process_image.py
+++ image_processing_backend/process_image.py
@@ -1,29 +1,70 @@
 # Imported PIL Library from PIL import Image
+from PIL import Image
+from IPython import embed
+
 
 # Open an Image
 def open_image(path):
-  newImage = Image.open(path)
-  return newImage
+    newImage = Image.open(path)
+    return newImage
+
 
 # Save Image
 def save_image(image, path):
-  image.save(path, 'png')
+    image.save(path, 'png')
+
 
 # Create a new image with the given size
 def create_image(i, j):
-  image = Image.new("RGB", (i, j), "white")
-  return image
+    image = Image.new("RGB", (i, j), "white")
+    return image
+
 
 # Get the pixel from the given image
 def get_pixel(image, i, j):
     # Inside image bounds?
     width, height = image.size
     if i > width or j > height:
-      return None
+        return None
 
     # Get Pixel
     pixel = image.getpixel((i, j))
     return pixel
 
+
+# Create a Grayscale version of the image
+def convert_grayscale(image):
+    # Get size
+    width, height = image.size
+
+    # Create new Image and a Pixel Map
+    new = create_image(width, height)
+    pixels = new.load()
+
+    # Transform to grayscale
+    for i in range(width):
+        for j in range(height):
+            # Get Pixel
+            pixel = get_pixel(image, i, j)
+
+            # Get R, G, B values (This are int from 0 to 255)
+            red = pixel[0]
+            green = pixel[1]
+            blue = pixel[2]
+
+            # Transform to grayscale
+            gray = (red * 0.299) + (green * 0.587) + (blue * 0.114)
+
+            # Set Pixel in new image
+            pixels[i, j] = (int(gray), int(gray), int(gray))
+
+    # Return new image
+    return new
+
+
 if __name__ == '__main__':
-  print('file runs')
+    path = '/Users/jaivignesh/Desktop/docusign/image_processing_backend/naturepic.jpg'
+    image = open_image(path)
+    modified_image = convert_grayscale(image)
+    modified_image.show()
+    # print('file runs')
